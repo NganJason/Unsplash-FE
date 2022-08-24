d6177ec52415071770260b24d44e13b65dd7b499
jason.ngan <jason.ngan@shopee.com>
2022-08-24 08:27:37 +0800

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Nav from './_shared/Components/Nav/Nav';
+
 function App() {
   return (
     <div className="App">
+        <Nav></Nav>
     </div>
   );
 }
---
+++ src/_shared/Components/Nav/s.module.scss.d.ts
@@ -0,0 +1,7 @@
+export const hamburger: string;
+export const nav: string;
+export const navBrand: string;
+export const navMiddle: string;
+export const navRight: string;
+export const navSetting: string;
+export const searchBar: string;
