6bf861d56ee75d45df9131de31dbef55a6ddbab9
jason.ngan <jason.ngan@shopee.com>
2022-08-27 16:23:00 +0800

    Add responsive grid

---
+++ src/_shared/hooks/useWindowDimensions.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const getWindowDimensions = (): { width: number; height: number } => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export const useWindowDimensions = (): { width: number; height: number } => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
