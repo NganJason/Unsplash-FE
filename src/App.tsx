18be85384b9ed62d82e98a08168e6db0feb22b7d
jason.ngan <jason.ngan@shopee.com>
2022-09-01 10:05:01 +0800

    add signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
 import LoginModal from './Components/LoginModal/LoginModal';
+import Signup from './Components/Signup/Signup';
 
 import Nav from './_shared/Components/Nav/Nav';
 
+const withNav = (component: JSX.Element): JSX.Element => {
+  return (
+    <>
+      <Nav/>
+      {component}
+    </>
+  );
+}
 function App() {
   return (
     <div className="App">
-        <Nav></Nav>
-        <Home/>
-        <LoginModal/>
+      <Routes>
+        <Route path="/" element={withNav(<Home />)} />
+        <Route path="/login" element={withNav(<Home />)} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+      <LoginModal />
     </div>
   );
 }
